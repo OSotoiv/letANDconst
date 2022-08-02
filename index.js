32ba59b0c98e4850aa854d97b1c68c98e77db41f
OctavianoSoto <osoto.iv@gmail.com>
2022-08-02 18:54:53 -0500

    let const var ES2015

---
+++ index.js
@@ -0,0 +1,18 @@
+var PI = 3.14;
+PI = 42; // stop me from doing this!
+
+/* Write an ES2015 Version */
+const pi = 3.14;
+
+// What is the difference between var and let?
+//let CANNOT BE REDECLARED. var IS SCOPED TO THE WHOLE FUNCTION
+//let IS SCOPED TO THE BLOCK OF CODE IT WAS DECLARED
+
+// What is the difference between var and const?
+//const CANNOT BE REASSIGNED OR REDECLARED
+
+// What is the difference between let and const?
+//let CAN BE REASSIGNED TO NEW VALUE WHILE const CANNOT
+
+// What is hoisting?
+//HOISTING IS WHEN JS DECLARES  "var" VARIABLES FIRST BEFORE USING THEM.
